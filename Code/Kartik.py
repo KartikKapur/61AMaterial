5b07176fe8ccfd4d97eba48abb14fad619c0089a
Kartik Kapur <kapurkartik1@gmail.com>
2017-09-17 14:10:48 -0700

    final midterm

---
+++ Code/Kartik.py
@@ -0,0 +1,11 @@
+karty, party = 5, 10
+def kart(ik):
+    kart = ik(karty)
+    while kart:
+        def ik():
+            return lambda kario, mart: 10
+        party = kart
+        kar = True == 6
+    return ik
+karty = lambda party: karty + kart(party)
+karty = karty(kart)
